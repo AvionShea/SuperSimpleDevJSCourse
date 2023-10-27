51dcce4f475777096285584bb119f789553a3a9a
Avion <avionsheadesigns@gmail.com>
2023-10-26 21:03:35 -0400

    added new files

---
+++ jsVariablePractice.js
@@ -0,0 +1,18 @@
+let variableOne = 3;
+console.log(variableOne);
+
+let calculation = 2 + 2;
+console.log(calculation);
+console.log(calculation + 2);
+
+let result = calculation + 2;
+console.log(result);
+
+let message = 'hello';
+console.log(message);
+
+variableOne = 5;
+console.log(variableOne);
+
+variableOne = variableOne + 1;
+console.log(variableOne)
