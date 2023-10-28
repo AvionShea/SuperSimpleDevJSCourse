a14f6d69f4a07814266eee6c04e06daf52bf01ad
Avion <avionsheadesigns@gmail.com>
2023-10-27 21:06:30 -0400

    jsVariablePractice.js file updates

--- jsVariablePractice.js
+++ jsVariablePractice.js
@@ -31,23 +31,23 @@ console.log(typeof message);
 //Lesson 5 Exercises - Variables
 
 //5a. Create a variable called 'name', and save your name in this variable.
-  const name = 'Avion'
+  const name = 'Avion';
 
 //5b. Display the message 'My name is:' in the console.
-  console.log(`My name is ${name}.`)
+  console.log(`My name is ${name}.`);
 
 //5c. At a restaurant, you order 1 coffee($5), 2 bagels($3 each), and 1 soup($9). Calculate the coast and save it in a variable called 'cost'.
-  let cost = (5 + (2*3) + 9)
+  let cost = (5 + (2*3) + 9);
 
 //5d. Continuing from 5c, display 'Cost of food:' in the console.
-  console.log(`Cost of food: $${cost}.`)
+  console.log(`Cost of food: $${cost}.`);
 
 //5e. Let's say the restaurant charges a 10% tax. Using the 'cost' variable from 5c, calculate the tax, and save the results in a variable.
-  let foodTaxes = cost * 0.01
+  let foodTaxes = cost * 0.01;
 
 //5f. Continuing from 5e, display 'Tax (10%):' in the console.
-  console.log(`Tax (10%): $${foodTaxes}`)
+  console.log(`Tax (10%): $${foodTaxes}`);
 
 //5g. Continuing from 5f, calculate the total (cost + tax), save it in a variable called 'totalCost', and display the message 'Total cost:' in the console.
   let totalCost = cost + foodTaxes
-  console.log(`Total cost: $${totalCost}.`)
+  console.log(`Total cost: $${totalCost}.`);
