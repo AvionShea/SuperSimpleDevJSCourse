b8c9e286ce03f629701c3e04cccb190f0f34f1c1
Avion <avionsheadesigns@gmail.com>
2023-10-31 19:57:46 -0400

    completion of lesson 07 exercise.

--- Lesson07/Lesson07_Exercises.js
+++ Lesson07/Lesson07_Exercises.js
@@ -34,3 +34,18 @@ function convertToCelsius(fahrenheit){
   return (fahrenheit - 32) * 5/9;
 }
 convertToCelsius(86);
+
+//7f.
+function convertTemperature(degrees, unit){
+  if(unit ==='C'){
+    const temps = convertToFahrenheit(degrees);
+    return `It's ${temps}°F.`;
+  }
+  else if(unit === 'F'){
+    const temps = convertToCelsius(degrees);
+    return `It's ${temps}°C.`;
+  }
+}
+
+console.log(convertTemperature(25, 'C'));
+console.log(convertTemperature(86, 'F'));
