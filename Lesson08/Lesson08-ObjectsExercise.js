eef2e12768e7100419eda8998332bf0f7a8f6a82
AvionShea <avionsheadesigns@gmail.com>
2023-11-09 09:39:19 -0500

    processing of Lesson 08 - Objects exercise.

--- Lesson08/Lesson08-ObjectsExercise.js
+++ Lesson08/Lesson08-ObjectsExercise.js
@@ -10,3 +10,26 @@ basketball.price += 500;
 console.log(basketball);
 
 //8c
+basketball['delivery-time'] = '3 days';
+
+//8d
+function comparePrice(productOne, productTwo){
+    if(productOne.price < productTwo.price){
+        return productOne;
+    }else {
+        return productTwo;
+    }
+};
+
+productOne = {
+    name: 'computer monitor',
+    price: 3000
+};
+productTwo = {
+    name: 'CPU',
+    price: 8000
+};
+
+console.log(comparePrice(productOne, productTwo));
+
+//8e
