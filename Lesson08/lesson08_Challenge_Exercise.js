014b069f31aa7aae8fae8d74acfaa489373362b9
AvionShea <avionsheadesigns@gmail.com>
2023-11-11 00:49:18 -0500

    Starter code for Lesson 08 - objects challenge exercise.

--- Lesson08/lesson08_Challenge_Exercise.js
+++ Lesson08/lesson08_Challenge_Exercise.js
@@ -1,7 +1,7 @@
 //Lesson 6 Exercises
 //6f - 6i
 
-let coinFlip = Math.random();
+/* let coinFlip = Math.random();
 
 if (coinFlip < 0.5){
   flipResults = 'Heads';
@@ -15,8 +15,9 @@ if(personGuess === flipResults){
   console.log('You Win!');
 }else{
   console.log('You Lose!');
-}
+} */
 
+let personGuess = 'Heads';
 
 let coinSide = (Math.random() < 0.5) ? 'Heads' : 'Tails';
 console.log(coinSide);
