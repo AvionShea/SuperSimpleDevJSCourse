2d128547ffb6ecf4930e23185c59529e9fdcb025
AvionShea <avionsheadesigns@gmail.com>
2023-11-15 08:25:44 -0500

    Progression of Lesson 09 - DOM.

--- Lesson09/lesson09_DOM.js
+++ Lesson09/lesson09_DOM.js
@@ -1,1 +1,20 @@
 //Lesson 09 JavaScript
+/*document.body.innerHTML = 'Hello';
+document.title = "Good job!"*/
+/*
+console.log(document.title);
+document.title = 'Changed';
+
+console.log(document.body);
+console.log(typeof document.body);
+
+console.log(document.body.innerHTML);
+document.body.innerHTML = '<button>Good Job!</button>';
+*/
+
+console.log(document.querySelector('button').innerHTML);
+document.querySelector('button')
+    .innerHTML = 'Changed';
+
+const buttonElement = document.querySelector('.js-button');
+console.log(buttonElement);
