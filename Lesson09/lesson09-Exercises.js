4a16d50bd22d9833bedf3afeed84e1938bc12647
AvionShea <avionsheadesigns@gmail.com>
2024-02-13 17:27:19 -0500

    completion of lesson09 main exercise

--- Lesson09/lesson09-Exercises.js
+++ Lesson09/lesson09-Exercises.js
@@ -3,3 +3,18 @@ console.log(document.querySelector('button'));
 //9b
 document.querySelector('.js-9b').innerText = '9b done!';
 
+//9d
+document.querySelector('#submitName').addEventListener('click', displayName);
+
+
+function displayName() {
+    let userName = document.querySelector('#userName').value
+
+    document.querySelector('#nameResults').innerHTML = `Your name is: ${userName}!`
+}
+
+function enterKeyUsed(event) {
+    if (event.key === 'Enter') {
+        displayName();
+    }
+};
