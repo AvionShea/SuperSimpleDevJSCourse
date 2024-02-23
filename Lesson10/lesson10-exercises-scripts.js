a58885d7b414529754bd1fff18e3ce1801a8656d
AvionShea <avionsheadesigns@gmail.com>
2024-02-23 08:28:21 -0500

    added pseudocode to js file

--- Lesson10/lesson10-exercises-scripts.js
+++ Lesson10/lesson10-exercises-scripts.js
@@ -1,3 +1,7 @@
 //10c
 let btn = document.querySelector('.js-button');
 console.log(btn.classList.contains('js-button'));
+
+//on click toggle button - .classList.add('.is-toggled')
+//check if toggle - .classList.contains('.is-toggled')
+//if toggle, turn off - .classList.remove('.is-toggled')
