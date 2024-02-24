0930573e157472bf7a960e991ed896c250e8afa6
AvionShea <avionsheadesigns@gmail.com>
2024-02-23 19:43:30 -0500

    completion of lesson 10 exercises

--- Lesson10/lesson10-exercises-scripts.js
+++ Lesson10/lesson10-exercises-scripts.js
@@ -5,3 +5,23 @@ console.log(btn.classList.contains('js-button'));
 //on click toggle button - .classList.add('.is-toggled')
 //check if toggle - .classList.contains('.is-toggled')
 //if toggle, turn off - .classList.remove('.is-toggled')
+
+//single button
+// let pressedBtn = document.querySelector('.toggling')
+// function toggleCheck(){
+//     if(!pressedBtn.classList.contains('is-toggled')){
+//         pressedBtn.classList.add('is-toggled')
+//     }else{
+//         pressedBtn.classList.remove('is-toggled')
+//     };
+// };
+
+//multiple buttons
+function toggleCheck(selector){
+    let pressedBtn = document.querySelector(selector);
+    if(!pressedBtn.classList.contains('is-toggled')){
+        pressedBtn.classList.add('is-toggled')
+    }else{
+        pressedBtn.classList.remove('is-toggled')
+    };
+};
